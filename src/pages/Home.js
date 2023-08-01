2e13dffa971debe88099e366ca1eee9f22c09b15
yesolee <dpthf9596@naver.com>
2023-08-01 22:46:58 +0900

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function home(){
+    return <h1>홈화면입니다.</h1>
+}
+
+export default home;
+
+
+// import react, {Component} from "react";
+
+// class Home extends Component {
+//     render() {
+//         return <h1>홈화면입니다</h1>
+//     }
+// }
+// export default Home;
